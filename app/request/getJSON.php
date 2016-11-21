54b53933ddcbfbad46d22d27580ccf78cdef2ada
Oğuzhan Demiröz <oguzhan@kodeveloper.co>
2016-11-22 01:41:45 +0300

    getjson hotfix

--- app/request/getJSON.php
+++ app/request/getJSON.php
@@ -4,8 +4,8 @@ try{
     if(isset($_GET["type"])) {
         $query = $db->select($_GET["type"])
             ->run();
-        #$json = json_encode($query, JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_QUOT | JSON_HEX_AMP);
-        print_r(json_decode(json_encode($json, JSON_PRETTY_PRINT)));
+        $json = json_encode($query, JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_QUOT | JSON_HEX_AMP);
+        print_r($json);
     }
 } catch (PDOException $e){
     print $e->getMessage();
